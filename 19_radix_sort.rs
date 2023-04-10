c1dfb488e1e6fd07f177f03c97207d1e59b8a5d2
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-04-10 21:01:37 +0600

    Create 19_radix_sort.rs

---
+++ 19_radix_sort.rs
@@ -0,0 +1,25 @@
+fn radix_sort(arr: &mut [u32]) {
+    let max = *arr.iter().max().unwrap();
+    let mut exp = 1;
+    let mut buffer = vec![0; arr.len()];
+
+    while max / exp > 0 {
+        let mut buckets = [0; 10];
+
+        for &num in arr.iter() {
+            buckets[(num / exp) as usize % 10] += 1;
+        }
+
+        for i in 1..10 {
+            buckets[i] += buckets[i - 1];
+        }
+
+        for &num in arr.iter().rev() {
+            buffer[buckets[(num / exp) as usize % 10] as usize - 1] = num;
+            buckets[(num / exp) as usize % 10] -= 1;
+        }
+
+        arr.copy_from_slice(&buffer);
+        exp *= 10;
+    }
+}
